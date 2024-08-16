f586c7444a09ebf3a5546e915fdb33eb7b3fc41a
mkreusch <info@kreusch-creative.de>
2024-08-16 19:33:45 +0200

    prevent errors when module is uploaded but not installed

--- files/GXModules/OnlineshopConsulting/AmazonPay/Admin/Overloads/OrderExtenderComponent/AmazonPay_OrderExtenderComponent.inc.php
+++ files/GXModules/OnlineshopConsulting/AmazonPay/Admin/Overloads/OrderExtenderComponent/AmazonPay_OrderExtenderComponent.inc.php
@@ -1,5 +1,7 @@
 <?php
 
+use OncoAmazonPay\LogService;
+
 class AmazonPay_OrderExtenderComponent extends AmazonPay_OrderExtenderComponent_parent
 {
     const ORDER_EXTENDER_POSITION = 'below_product_data';
@@ -9,19 +11,22 @@ class AmazonPay_OrderExtenderComponent extends AmazonPay_OrderExtenderComponent_
         parent::proceed();
         $orderId = (int)$this->v_data_array['GET']['oID'];
         $transactionService = new \OncoAmazonPay\TransactionService();
+        try {
+            $transactions = $transactionService->getTransactionsOfOrder($orderId);
 
-        $transactions = $transactionService->getTransactionsOfOrder($orderId);
-
-        if (!empty($transactions)) {
-            define('HAS_AMAZON_PAY_TRANSACTIONS', true);
-            $heading = '<img src="' . DIR_WS_CATALOG . 'GXModules/OnlineshopConsulting/AmazonPay/Resources/image/logo_full.svg" alt="Amazon Pay" style="height: 18px; margin-top: 3px;" />';
-            $body = '<div id="admin-amazon-pay-transactions" 
+            if (!empty($transactions)) {
+                define('HAS_AMAZON_PAY_TRANSACTIONS', true);
+                $heading = '<img src="' . DIR_WS_CATALOG . 'GXModules/OnlineshopConsulting/AmazonPay/Resources/image/logo_full.svg" alt="Amazon Pay" style="height: 18px; margin-top: 3px;" />';
+                $body = '<div id="admin-amazon-pay-transactions" 
                           data-load-url="' . xtc_href_link('admin.php', 'do=AmazonPayTransactions/getTransactions&orderId=' . $orderId) . '"
                           data-refresh-url="' . xtc_href_link('admin.php', 'do=AmazonPayTransactions/refresh') . '"
                           data-action-url="' . xtc_href_link('admin.php', 'do=AmazonPayTransactions/doAction') . '"
                           data-order-id="' . $orderId . '"
                      ></div>';
-            $this->addContentToCollection(self::ORDER_EXTENDER_POSITION, $body, $heading);
+                $this->addContentToCollection(self::ORDER_EXTENDER_POSITION, $body, $heading);
+            }
+        } catch (Exception $e) {
+            (new LogService())->error('AmazonPay_OrderExtenderComponent error: ' . $e->getMessage());
         }
     }
 }
